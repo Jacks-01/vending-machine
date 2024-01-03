6acbac7eb2d69a28e21f4e6cf17d4c166a654c19
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2024-01-03 16:50:31 -0600

    jeffrey bezos!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { Alert, Button, Container, Form, Image, Stack } from 'react-bootstrap';
+import { Alert, Button, Container, Image, Stack } from 'react-bootstrap';
 import jeff from './assets/jeff.png';
 import skittles from './assets/skittles.png';
 import mm from './assets/mm.png';
 import snickers from './assets/snickers.png';
 import vendingMachine from './assets/vending.svg';
-import sourcelogo from './assets/sourcelogo.jpg';
 
 import './App.css';
 import { useState } from 'react';
@@ -50,8 +49,8 @@ function App() {
 		let change = 0;
 
 		if (price > 100) {
-      setDispensedCandy(jeff);
-      setMessage('Jeffrey Bezos')
+			setDispensedCandy(jeff);
+			setMessage('Jeffrey Bezos');
 			return;
 		}
 
@@ -78,83 +77,86 @@ function App() {
 
 	return (
 		<>
-			{message && 
-				<Alert variant={message === 'Jeffrey Bezos' ? 'success' : 'danger'}>
-          <Alert.Heading>{message}</Alert.Heading>
-          {message === 'Jeffrey Bezos' ? <Alert.Link href='https://www.youtube.com/watch?v=ssbPGD2OqRE' target='_blank'> 🎶 Jeffrey Bezos 🎶 </Alert.Link> : <></>}
+			
+			<div style={{}}>
+			{message && (
+				<Alert
+					variant={
+						message === 'Jeffrey Bezos' ? 'success' : 'danger'
+					}>
+					<Alert.Heading>{message}</Alert.Heading>
+					{message === 'Jeffrey Bezos' ? (
+						<Alert.Link
+							href='https://www.youtube.com/watch?v=ssbPGD2OqRE'
+							target='_blank'>
+							{' '}
+							🎶 Jeffrey Bezos 🎶{' '}
+						</Alert.Link>
+					) : (
+						<></>
+					)}
 				</Alert>
-			}
-
-			<div
-				style={{
-					width: 100,
-					zIndex: 3,
-					marginLeft: '-50px',
-					position: 'absolute',
-					right: 60,
-					top: 250,
-				}}>
-				<Stack
-					direction='vertical'
-					gap={3}>
-					<Button onClick={() => setInputCandy('Skittles')}>
-						Skittles
-					</Button>
-					<Button onClick={() => setInputCandy('Mini M&M')}>
-						M&M's
-					</Button>
-					<Button onClick={() => setInputCandy('Snickers')}>
-						Snickers
-					</Button>
-					<input
-						type='number'
-						step={0.5}
-						onChange={(e) =>
-							setInputMoney(parseFloat(e.target.value))
-						}
-					/>
-					<Button
-						onClick={vendingSubmitHandler}
-						style={{ backgroundColor: 'green' }}>
-						Dispense
-					</Button>
-				</Stack>
-			</div>
-			<Image
-				rounded
-				src={sourcelogo}
-				style={{
-					width: 60,
-					position: 'absolute',
-					right: 220,
-					top: 130,
-					zIndex: 3,
-				}}
-			/>
-			<Image
-				src={vendingMachine}
-				width={300}
-				style={{
-					zIndex: 0,
-					position: 'absolute',
-					bottom: 0,
-					right: 100,
-				}}
-			/>
-
-			{dispensedCandy !== '' && (
-				<Image
-					src={dispensedCandy}
+			)}
+				<div
 					style={{
+						width: 100,
+						zIndex: 3,
 						position: 'absolute',
-						zIndex: 5,
-						bottom: 15,
-						right: 160,
-						width: 200,
-						height: 150,
+						right: '23%',
+						bottom: '15%',
+					}}>
+					<Stack
+						direction='vertical'
+						gap={3}>
+						<Button onClick={() => setInputCandy('Skittles')}>
+							Skittles
+						</Button>
+						<Button onClick={() => setInputCandy('Mini M&M')}>
+							M&M's
+						</Button>
+						<Button onClick={() => setInputCandy('Snickers')}>
+							Snickers
+						</Button>
+						<input
+							type='number'
+							step={0.5}
+							onChange={(e) =>
+								setInputMoney(parseFloat(e.target.value))
+							}
+						/>
+						<Button
+							onClick={vendingSubmitHandler}
+							style={{ backgroundColor: 'green' }}>
+							Dispense
+						</Button>
+					</Stack>
+				</div>
+
+				<Image
+					src={vendingMachine}
+					width={300}
+					style={{
+						zIndex: 0,
+						position: 'absolute',
+						bottom: '0%',
+						right: '25%',
 					}}
 				/>
-			)}
+
+				{dispensedCandy !== '' && (
+					<Image
+						src={dispensedCandy}
+						style={{
+							position: 'absolute',
+							zIndex: 5,
+							bottom: '2%',
+							right: '28%',
+							width: 200,
+							height: 150,
+						}}
+					/>
+				)}
+			</div>
 		</>
 	);
 }
